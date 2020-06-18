95dd3c9a07dc63c472ddb9e8ff0fe715dd702bea
Akhromova Julia <juliheksey@gmail.com>
2020-06-18 03:04:53 +0300

    initial

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const config = require('config');
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
+const path = require('path');
 
 const app = express();
 
 //чтобы body запроса не было undefined парсим в json
 app.use(express.json({extended: true}));
-
 app.use('/api/auth', require('./routes/auth.routes'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = config.get('port') || 5000;
 const HOST = config.get('host') || "localhost";
